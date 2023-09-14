c7455e280d94ce8c2257337bf82f855aeb7646ee
forcepusher <bionitsoup@gmail.com>
2023-09-14 19:53:37 +0700

    Character controller

--- Assets/Source/PlayerCharacter.cs
+++ Assets/Source/PlayerCharacter.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
-    private void Update()
+    private const float TorquePerSecond = 500f;
+    private const float MaximumAngularVelocity = 10f;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float input = Input.GetAxisRaw("Horizontal");
+        _rigidbody.AddTorque(-input * Time.deltaTime * TorquePerSecond);
+
     }
 }
