92dc5f297f1286b56e610809f15cebc5bdcf6781
forcepusher <bionitsoup@gmail.com>
2023-09-14 21:00:24 +0700

    Tweak sizes

--- Assets/Source/Scripts/PlayerCharacter.cs
+++ Assets/Source/Scripts/PlayerCharacter.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
-    private const float TorquePerSecond = 20000f;
+    private const float TorquePerSecond = 1000f;
 
     private Rigidbody2D _rigidbody;
 
@@ -14,13 +14,13 @@ public class PlayerCharacter : MonoBehaviour
     private void FixedUpdate()
     {
         float input = Input.GetAxisRaw("Horizontal");
-        _rigidbody.AddTorque(-input * Time.deltaTime * TorquePerSecond);
+        _rigidbody.AddTorque(-input * Time.deltaTime * TorquePerSecond * _rigidbody.mass);
     }
 
     private void Update()
     {
         bool jumpInput = Input.GetButtonDown("Jump");
         if (jumpInput)
-            _rigidbody.AddForce(new Vector2(0f, 400f * _rigidbody.mass));
+            _rigidbody.AddForce(new Vector2(0f, 300f * _rigidbody.mass));
     }
 }
